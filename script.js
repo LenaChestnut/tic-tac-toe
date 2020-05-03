752b5c29e406539a83366605c2adde37cf939a2e
Elena Kashtanova <lenachestnut@gmail.com>
2020-05-03 20:54:11 +0300

    Implement minimax for AI

--- script.js
+++ script.js
@@ -186,14 +186,14 @@ const gameModule = (() => {
                 if (gameStarted) {
                     if (isEmpty(cells[i])) {
                         markCell(cells[i]);
-                        roundEnd(cells, cells[i]);
+                        endRound(cells, cells[i]);
                     }    
                 }
             });
         }
     };
 
-    const roundEnd = (grid, cell) => {
+    const endRound = (grid, cell) => {
         if (hasWon(grid, cell)) {
             gameStarted = false;
             const winningComb = getWinningComb(cell, grid);
@@ -219,26 +219,83 @@ const gameModule = (() => {
         }
     };
 
+    const getBestSpot = (cells) => {
+        return minimax(cells, gameModule.activePlayer).index;
+    };
+
     const makeAIMove = () => {
         setTimeout(function() {
             const allCells = Array.from(document.querySelectorAll(".cell"));
-            const emptyCells = allCells.filter(isEmpty);
-            const num = getRandom(emptyCells);
-            
-            markCell(emptyCells[num]);
+            let bestSpot = getBestSpot(allCells);
+            markCell(bestSpot);
 
-            roundEnd(allCells, emptyCells[num]);
-        }, 200);
+            endRound(allCells, bestSpot);
+        }, 250);
     };
 
-    const getRandom = (arr) => {
-        return Math.floor(Math.random() * arr.length);
+    const minimax = (board, player, cell) => {
+        let availableCells = board.filter(isEmpty);
+
+        if (cell && player.isHuman() && hasWon(board, cell)) {
+            return {score: 10};
+        } else if (cell && !player.isHuman() && hasWon(board, cell)) {
+            return {score: -10};
+        } else if (availableCells.length === 0) {
+            return {score: 0};
+        }
+
+        const moves = [];
+
+        for (let i = 0; i < availableCells.length; i++) {
+            let move = {};
+            currentMove = board[board.indexOf(availableCells[i])];
+            move.index = currentMove;
+
+            currentMove.textContent = player.getMarker();
+
+            if (player == playerX) {
+                let result = minimax(board, playerO, currentMove);
+                move.score = result.score;
+            } else if (player == playerO) {
+                let result = minimax(board, playerX, currentMove);
+                move.score = result.score;
+            }
+
+            availableCells.forEach((cell) => {
+                cell.textContent = "";
+            });
+
+            moves.push(move);
+        }
+
+        let bestMove;
+        if (!player.isHuman()) {
+            let bestScore = -1000;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score > bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        } else {
+            let bestScore = 1000;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score < bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+
+        return moves[bestMove];
     };
+
 
     const isEmpty = (cell) => {
         if (!cell.textContent) {
             return true;
         }
+        return false;
     };
 
     const markCell = (cell) => {
@@ -254,10 +311,21 @@ const gameModule = (() => {
         const currentRow = getCurrentRow(clickedCell, cellsArr);
         const currentColumn = getCurrentColumn(clickedCell, cellsArr);
 
-        if (currentRow.every(hasSameMarker) || currentColumn.every(hasSameMarker) ||
-        d1.every(hasSameMarker) || d2.every(hasSameMarker)) {
+        const checkWin = (comb, cell) => {
+            const marker = cell.textContent;
+            let checkResult = comb.every((item) => {
+                if (item.textContent === marker) {
+                    return true;
+                }
+                return false;
+            });
+            return checkResult;
+        };
+
+        if (checkWin(currentRow, clickedCell) || checkWin(currentColumn, clickedCell) || checkWin(d1, clickedCell) || checkWin(currentRow, clickedCell)) {
             return true;
         }
+        return false;
     };
 
     const hasSameMarker = (currentCell) => {
